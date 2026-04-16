3529b916c1c3b3f246b074ec4e8a5eb96dd07d80
magarahayato <magara_hayato@koushi-intec.co.jp>
2026-04-16 11:59:22 +0900

    Add files via upload

--- kadai_015/CarExec_Chapter15.java
+++ kadai_015/CarExec_Chapter15.java
@@ -1,24 +1,13 @@
 package kadai_015;
 
 public class CarExec_Chapter15 {
-	private int gear=1;
-	private int speed=10;
-
-	public void changeGear(int afterGear) {
-		speed=switch (afterGear) {
-		case 1 ->10;
-		case 2 ->20;
-		case 3 ->30;
-		case 4 ->40;
-		case 5 ->50;
-		default ->10;
-		};
-		System.out.println("ギア"+this.gear+"から"+afterGear+"切り替えました");
+	public static void main(String[] args) {
+		Car_Chapter15 a = new Car_Chapter15( );
+		
+		a.changeGear(3);
+		
+		a.run();
 	}
-	
-		public void run() {
-			System.out.println("速度は時速"+speed+"kmです");
-		}
 }
 	
 
--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -1,12 +1,23 @@
 package kadai_015;
 
 public class Car_Chapter15 {
-	public static void main(String[] args) {
-		CarExec_Chapter15 a = new CarExec_Chapter15( );
-		
-		a.changeGear(3);
-		
-		a.run();
+	private int gear=1;
+	private int speed=10;
+
+	public void changeGear(int afterGear) {
+		speed=switch (afterGear) {
+		case 1 ->10;
+		case 2 ->20;
+		case 3 ->30;
+		case 4 ->40;
+		case 5 ->50;
+		default ->10;
+		};
+		System.out.println("ギア"+this.gear+"から"+afterGear+"に切り替えました");
 	}
+	
+		public void run() {
+			System.out.println("速度は時速"+speed+"kmです");
+		}
 }
 	
