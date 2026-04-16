19b4385112c4eb06eb67f468bd0b2a0866b23665
magarahayato <magara_hayato@koushi-intec.co.jp>
2026-04-16 11:41:24 +0900

    Add files via upload

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,12 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+	public static void main(String[] args) {
+		CarExec_Chapter15 a = new CarExec_Chapter15( );
+		
+		a.changeGear(3);
+		
+		a.run();
+	}
+}
+	
